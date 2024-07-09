6428604d4176de5e97306d238deee23c2fa0efbf
Vorshim92 <vorshim92@gmail.com>
2024-07-09 21:42:54 +0200

    just scss

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <h1>STEFANO SCALFARI</h1>
-        <h2>SITO IN MANUTENZIONE TESTING BRANCH</h2>
+      <div className="container vh-100 d-flex justify-content-center align-items-center">
+        <div className="row">
+          <div className="col">
+            <div className="text-center">
+              <h1 className="text-white">STEFANO SCALFARI</h1>
+              <h2>UNDER CONSTRUCTION</h2>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
